b0819bb9e244617e12a51b86b32e457b37d1f682
pavanraju13 <pavanraju4513@gmail.com>
2025-01-28 18:21:42 +0530

    interconnection

--- Backend.sh
+++ Backend.sh
@@ -77,7 +77,7 @@ VALIDATE $? "Enabling backend"
 dnf install mysql -y &>>$LOGFILE
 VALIDATE $? "Installing MySQL Client"
 
-mysql -h 172.31.25.0 -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+mysql -h db.clouddevops.life -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
 VALIDATE $? "Schema loading"
 
 systemctl restart backend &>>$LOGFILE
